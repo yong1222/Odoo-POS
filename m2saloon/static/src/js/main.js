d0bae105927da616cbdca231285c4fe4df03c79e
yong1222 <yong1222@users.noreply.github.com>
2017-04-04 18:37:14 +0800

    Add files via upload

---
+++ m2saloon/static/src/js/main.js
@@ -0,0 +1,7 @@
+openerp.m2saloon = function(instance){
+
+    var module = instance.point_of_sale;
+
+    openerp_m2saloon_salesperson(instance,module);
+    openerp_m2saloon_assistant(instance,module);
+};

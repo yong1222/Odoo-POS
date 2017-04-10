e60c68b4d89f02b3ebd1e0e8c05740aab98e57c8
yong1222 <yong1222@users.noreply.github.com>
2017-04-10 12:51:26 +0800

    Add files via upload

---
+++ m2saloon_pos_form/__openerp__.py
@@ -0,0 +1,12 @@
+{
+    'name': "M2 POS Form",
+    'version': '1.0.0',
+    'author': 'ssy',
+    'category': 'Point_of_sale',
+    'website': 'http://www.itsmart.my/',
+    'depends': ['point_of_sale'],
+    'data': [
+        'views.xml',
+        ],
+    'installable': True,
+}

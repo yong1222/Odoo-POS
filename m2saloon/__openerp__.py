0a35d1ea552b07ead7d939936c314090c62fbb40
yong1222 <yong1222@users.noreply.github.com>
2017-04-19 09:44:25 +0800

    Update __openerp__.py

--- m2saloon/__openerp__.py
+++ m2saloon/__openerp__.py
@@ -1,16 +1,20 @@
 {
     'name': "POS M2 Sales Person",
-    'version': '0.1',
+    'version': '1.0.0',
     'author': 'ITSMART - SY',
     'category': 'Point of Sale',
     'website': 'https://www.itsmart.my',
     'depends': ['point_of_sale'],
+    'description':"""
+    This module will add salesperson and sales assistants into each POS orderline.
+    Add two function into POS system which allow cashier to assign Salesperson and Assistants into the Order/Invoice 
+    """,
     'data': [
         'views.xml',
     ],
     'qweb': [
-        'static/src/xml/assistant.xml',
-        'static/src/xml/salesperson.xml',
+        'static/src/xml/*.xml',
     ],
     'installable': True,
+    'application': True,
 }
